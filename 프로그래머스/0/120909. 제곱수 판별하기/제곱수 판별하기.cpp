0296a9dfb33b272b1ada014a28d479046bf414c8
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-05 23:42:57 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -5,18 +5,10 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    // 제곱근을 계산
-    double sqrt_n = sqrt(n);
-    
-    // floor 주어진 숫자를 내림하여 가장 가까운 정수로 만듬
-    // 제곱근이 정수인지 확인한다
-    if(sqrt_n == floor(sqrt_n))
-    {
-        return 1;
-    } else {
-        return 2;
+int answer = 0;
+    for(int i = 0; i * i <= n; i++){
+        if(i*i == n) return 1;
     }
-    
-    return answer;
+
+    return 2;
 }
