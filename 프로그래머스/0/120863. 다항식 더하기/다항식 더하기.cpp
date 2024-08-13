795205f51cca2e668faaeb4bc5abe0ed00387641
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-13 13:37:28 +0900

    [level 0] Title: 다항식 더하기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cpp
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.cpp
@@ -4,41 +4,28 @@
 
 using namespace std;
 
-string solution(string polynomial) {
-    int coefficient = 0; // x의 계수 합
-    int constant = 0;    // 상수항 합
+string solution(string p) {
+    int x = 0; // x의 계수 합
+    int c = 0; // 상수항 합
     
-    stringstream ss(polynomial);
+    stringstream ss(p);
     string term;
     
     while (ss >> term) {
         if (term == "+") continue; // '+'는 건너뜀
-        
-        // x가 포함된 항인지 확인
-        if (term.find('x') != string::npos) {
-            // x 앞에 숫자가 없는 경우 (계수 1인 경우)
-            if (term == "x") {
-                coefficient += 1;
-            } else {
-                // x 앞에 숫자가 있는 경우
-                coefficient += stoi(term.substr(0, term.find('x')));
-            }
-        } else {
-            // 상수항인 경우
-            constant += stoi(term);
-        }
+        if (term.find('x') != string::npos) { // x가 포함된 항인지 확인
+            if (term == "x") { x += 1; } // x 앞에 숫자가 없는 경우 (계수 1인 경우)
+            else { x += stoi(term.substr(0, term.find('x'))); } //x 앞 숫자가 있는 경우
+            } 
+        else { c += stoi(term); } // 상수항인 경우
     }
     
     // 결과를 생성
     string result = "";
-    
-    if (coefficient != 0) {
-        result += (coefficient == 1) ? "x" : to_string(coefficient) + "x";
-    }
-    
-    if (constant != 0) {
-        if (!result.empty()) result += " + ";
-        result += to_string(constant);
+    if (x != 0) { result += (x == 1) ? "x" : to_string(x) + "x"; }
+    if (c != 0) {
+    if (!result.empty()) result += " + ";
+        result += to_string(c);
     }
     
     return result;
