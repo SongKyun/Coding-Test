2c5b976aa84656f2561d5016f82a7c3d1c40358a
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-17 00:43:58 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.cpp
@@ -1,20 +1,14 @@
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> sides) {
-    sort(sides.begin(), sides.end()); // 두 변을 오름차순으로 정렬
+    sort(sides.begin(), sides.end());
+    
+    // 가능한 세 번째 변의 개수 계산
+    int answer = (sides[0] + sides[1] - 1) - (sides[1] - sides[0]);
 
-    int min_side = sides[0];
-    int max_side = sides[1];
-
-    // 1. x가 max_side보다 작은 경우
-    int count1 = max_side - (max_side - min_side); // max_side보다 작은 경우의 수
-
-    // 2. x가 max_side보다 큰 경우
-    int count2 = (min_side + max_side) - max_side - 1;
-
-    // 두 경우의 수를 더하여 반환
-    return count1 + count2;
+    return answer;
 }
