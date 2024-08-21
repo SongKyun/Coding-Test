701c4b6aa8d0d3b24816ec3b4234447525dce5ad
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-21 23:21:05 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -16,7 +16,7 @@ int main()
     }
     
     for(int i = 0; i < 26; i++)
-    if(cnt[i] >= 5) ret += (i + 'a'); // 해당 알파벳의 개수가 5개 이상이면 ret 문자열에 그 알파벳을 추가
+    if(cnt[i] >= 5) ret += (i + 'a'); // 5개 이상이면 ret 문자열에 그 알파벳을 추가
     
     if(ret.size()) cout << ret << "\n"; // 문자열이 비어 있지 않으면 그 문자열을 출력
     else cout << "PREDAJA" << "\n";
