f36b984d2bd1436f89a26fd21d0966240ffc36a0
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-27 02:19:42 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,51 +1,39 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <algorithm>
+
 using namespace std;
 
+string s, ret;
+int cnt[200], flag;
+char mid;
+
 int main() {
-    string name;
-    cin >> name;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> s;
 
-    map<char, int> char_count;
-    for (char c : name) { // 문자가 몇 번 등장하는지 카운팅
-        char_count[c]++;
-    }
+	for (char a : s)cnt[a]++; // 카운팅 배열 - 몇개 인가?
 
-    int odd_count = 0;
-    char odd_char = 0;
-    string first_half = "", second_half = "";
+	for (int i = 'Z'; i >= 'A'; i--) { // 오름 차순을 위해 z 부터
+		if (cnt[i]) {
+			if (cnt[i] & 1) // 비트연산 &(AND) : 마지막 비트가 1인 경우 홀수 , 0이면 짝수
+			{
+				mid = char(i); flag++; // 홀수인 경우 mid에 초기화 및 카운팅
+				cnt[i]--; // 홀수에서 -1 해서 짝수로 만든다 양 끝에 대칭으로 배치해야 하기 때문에
+			}
 
-    // 홀수 개수의 문자가 있는지 확인
-    for (auto& entry : char_count) {
-        if (entry.second % 2 != 0) {
-            odd_count++;
-            odd_char = entry.first;
-        }
-        // 홀수 개 문자가 2개 이상이면 팰린드롬 불가능
-        if (odd_count > 1) {
-            cout << "I'm Sorry Hansoo" << endl;
-            return 0;
-        }
-    }
+			if (flag == 2)break; // 홀수 2개인 경우 종료
 
-    // 팰린드롬의 절반 만들기
-    for (auto& entry : char_count) {
-        string temp(entry.second / 2, entry.first);
-        first_half += temp;
-    }
+			for (int j = 0; j < cnt[i]; j += 2) { // 2번 붙이므로 +2
+				ret = char(i) + ret; // 앞에 붙이고
+				ret += char(i); // 뒤에 붙이고
+			}
+		}
+	}
 
-    second_half = first_half;
-    reverse(second_half.begin(), second_half.end());
+	if (mid)ret.insert(ret.begin() + ret.size() / 2, mid); // 중앙에 mid 삽입
 
-    // 팰린드롬 완성
-    if (odd_count == 1) { // 홀수 개수 중앙 배치
-        cout << first_half + odd_char + second_half << endl;
-    }
-    else {
-        cout << first_half + second_half << endl;
-    }
+	if (flag == 2)cout << "I'm Sorry Hansoo\n";
 
-    return 0;
+	else cout << ret << "\n";
 }
