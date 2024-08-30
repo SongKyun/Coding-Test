e2d3d381dadabb95ad26d442c19c801a49581dd1
Song-Kyun <89376275+SongKyun@users.noreply.github.com>
2024-08-30 16:54:41 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,31 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <unordered_map>
-#include <map>
-#include <stack>
-#include <cmath>
-
+#include<bits/stdc++.h> 
 using namespace std;
-
-// 거듭제곱을 모듈러 연산을 적용하여 구하는 함수
-long long modular_pow(long long a, long long b, long long c) {
-    if (b == 0) return 1;  // A^0 = 1 (모든 수의 0제곱은 1)
-    long long half = modular_pow(a, b / 2, c);  // A^(B/2) 구하기
-    half = (half * half) % c;  // (A^(B/2))^2 % C 계산
-
-    if (b % 2 == 1) {  // 만약 B가 홀수라면
-        half = (half * a) % c;  // A * A^(B-1) % C 계산
-    }
-
-    return half;
+typedef long long ll;
+ll a, b, c;
+ll go(ll a, ll b){
+    if(b == 1) return a % c;
+    ll ret = go(a, b / 2);
+    ret = (ret * ret) % c;
+	if(b % 2)ret = (ret * a)% c;
+    return ret;
 }
-
 int main() {
-    long long A, B, C;
-    cin >> A >> B >> C;
-
-    cout << modular_pow(A, B, C) << endl;  // 결과 출력
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> a >> b >> c;
+    cout << go(a, b) << "\n";
     return 0;
 }
