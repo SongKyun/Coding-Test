8b7e1dd0da06783a5a2b0fbae4e0dec0cdef3916
Song Kyun <89376275+SongKyun@users.noreply.github.com>
2024-09-07 02:01:55 +0900

    [Silver IV] Title: 제로, Time: 24 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,31 +1,39 @@
 #include <iostream>
 #include <stack>
 
+using namespace std;
+
 int main() {
-    int K; // 입력 개수
-    std::cin >> K;
+	int K;
+	cin >> K;
 
-    std::stack<int> numbers; // 스택 자료구조를 사용하여 입력 수를 저장
+	stack<int> numbers; // 스택 자료구조 사용 , 입력 수 저장
 
-    for (int i = 0; i < K; ++i) {
-        int num;
-        std::cin >> num; // 입력 숫자를 받음
+	for (int i = 0; i < K; ++i)
+	{
+		int num;
+		cin >> num; // 입력 숫자 받음
 
-        if (num == 0) {
-            if (!numbers.empty()) {
-                numbers.pop(); // 0이 입력되면 가장 최근 수 제거
-            }
-        } else {
-            numbers.push(num); // 0이 아닌 경우 수를 스택에 추가
+		if (num == 0) {
+			if (!numbers.empty()) {
+				numbers.pop();
+			}
         }
-    }
+		else
+		{
+			// 0이 아닌 경우 수 추가
+			numbers.push(num);
+		}
+	}
 
-    int sum = 0; // 결과 합
-    while (!numbers.empty()) {
-        sum += numbers.top(); // 스택의 맨 위 요소를 더함
-        numbers.pop(); // 스택에서 요소 제거
-    }
+	int sum = 0;
+	while (!numbers.empty())
+	{
+		sum += numbers.top(); // 맨 위를 더하고
+		numbers.pop(); // 빼버림
+		// 반복하면서 모든 수를 더함
+	}
 
-    std::cout << sum << std::endl; // 결과 출력
-    return 0;
+	cout << sum << "\n";
+	return 0;
 }
