c190805e9fd967e8b07a2aeadb4f24b58c9dccbf
Song Kyun <89376275+SongKyun@users.noreply.github.com>
2024-09-12 22:02:57 +0900

    [Silver III] Title: 안테나, Time: 80 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.cc
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -4,21 +4,20 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<int> houses(N);
+	int N;
+	cin >> N;
+	vector<int> houses(N);
 
-    for (int i = 0; i < N; i++) {
-        cin >> houses[i];
-    }
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> houses[i];
+	}
 
-    // 집의 위치를 정렬합니다.
-    sort(houses.begin(), houses.end());
+	sort(houses.begin(), houses.end());
 
-    // 중앙값에 해당하는 위치에 안테나를 설치합니다.
-    int antenna_position = houses[(N - 1) / 2];
+	int antenna_position = houses[(N - 1) / 2];
 
-    cout << antenna_position << endl;
+	cout << antenna_position << endl;
 
-    return 0;
+	return 0;
 }
