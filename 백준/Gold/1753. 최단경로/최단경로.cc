476ccd18332506c1ba38fe2ef8514c8da45c6d17
Song Kyun <89376275+SongKyun@users.noreply.github.com>
2025-03-15 22:55:53 +0900

    [Gold IV] Title: 최단경로, Time: 88 ms, Memory: 9192 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,40 +1,49 @@
-#include <cstdio> 
-#include <algorithm> 
+#include <iostream>
 #include <vector>
 #include <queue>
-using namespace std; 
-int V, E, K, u, v, w; 
-vector<pair<int, int>> adj[20001]; 
-int dist[20001];
-bool visited[20001];
-const int INF = 987654321; 
-priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
-int main(){
-	scanf("%d %d %d", &V, &E, &K);
-	fill(dist, dist + 20001, INF);
-	for(int i = 0; i < E; i++){
-		scanf("%d %d %d", &u, &v, &w);
-		adj[u].push_back(make_pair(w, v)); 
-	}
-	pq.push(make_pair(0, K));
-	dist[K] = 0; 
-	while(pq.size()){ 
-		int here = pq.top().second;
-		int here_dist = pq.top().first;
-		pq.pop();
-		if(dist[here] != here_dist) continue;
-		for(pair<int, int> there : adj[here]){
-			int _dist = there.first;  
-			int _there = there.second; 
-			if(dist[_there] > dist[here] + _dist){
-				dist[_there] = dist[here] + _dist; 
-				pq.push(make_pair(dist[_there], _there));
-			}  
-		}
-	} 
-	for(int i = 1; i <= V; i++){ 
-		if(dist[i] == INF) puts("INF");
-		else printf("%d\n", dist[i]); 
-	}
-	return 0;
+using namespace std;
+
+const int INF = 1e9;  // 10억: 충분히 큰 값으로 INF 설정
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int V, E, K;
+    cin >> V >> E >> K;
+
+    vector<vector<pair<int, int>>> adj(V + 1);  // 정점 번호 1~V 사용
+    vector<int> dist(V + 1, INF);  // 거리 배열, INF로 초기화
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].emplace_back(w, v);  // (가중치, 도착점) 저장
+    }
+
+    pq.emplace(0, K);
+    dist[K] = 0;
+
+    while (!pq.empty()) {
+        int here = pq.top().second;
+        int here_dist = pq.top().first;
+        pq.pop();
+
+        if (dist[here] < here_dist) continue;  // 불필요한 경로 무시
+
+        for (auto& [w, there] : adj[here]) {
+            if (dist[there] > dist[here] + w) {
+                dist[there] = dist[here] + w;
+                pq.emplace(dist[there], there);
+            }
+        }
+    }
+
+    for (int i = 1; i <= V; i++) {
+        if (dist[i] == INF) cout << "INF\n";
+        else cout << dist[i] << "\n";
+    }
+
+    return 0;
 }
