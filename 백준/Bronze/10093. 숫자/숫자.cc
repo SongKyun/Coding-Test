1265ee3534d8832f18ba049da7bc736e574b82aa
SongKyun <89376275+SongKyun@users.noreply.github.com>
2025-10-07 14:06:18 +0900

    [Bronze II] Title: 숫자, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -8,20 +8,21 @@ int main()
     // 1. 2개 양의 정수 A B
     long long A, B;
     cin >> A >> B;
-    // 2. A와 B 둘 중 누가 더 큰지 판별
-    int big = max(A, B);
-    int small = min(A, B);
-    // 3. 예외 처리
+
+    // 2. 예외 처리 ( 두 수 같을 경우 )
     if (A == B) {   cout << 0 << "\n"; return 0;}
+
+    // 3. A와 B 둘 중 누가 더 큰지 판별
+    long long big = max(A, B);
+    long long small = min(A, B);
+
     // 4. 두 수 사이의 개수
-    int diff = big - small - 1;
-    // 5. 사이의 수를 벡터에 저장
-    vector<long long> v;
-    for (long long i = small + 1; i < big; i++) { v.push_back(i); }
-    // 6. 수의 개수 출력
+    long long diff = big - small - 1;
+
+    // 5. 수의 개수 출력
     cout << diff << "\n";
-    // 7. 오름차순 출력
-    sort(v.begin(), v.end());
-    for (long long num : v) cout << num << " ";
+    
+    // 5. 사이의 수를 벡터에 저장
+    for (long long i = small + 1; i < big; i++) { cout << i << " "; }
     return 0;
 }
