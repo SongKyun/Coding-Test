2a9b183a7a4eb5fba375a9b8e57acd94219c0bf7
SongKyun <89376275+SongKyun@users.noreply.github.com>
2025-10-13 22:16:53 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int N;
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 1; j <= N - i - 1; j++)
+        {
+            cout << ' ';
+        }
+        
+        for (int k = 0; k <= i; k++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
