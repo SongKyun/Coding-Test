ff786ace1314c261ec0bae24482221caedaf525f
SongKyun <89376275+SongKyun@users.noreply.github.com>
2026-01-15 20:14:02 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,39 +1,30 @@
 #include <iostream>
+#include <list>
 #include <stack>
-
+#include <vector>
 using namespace std;
 
+int ans, K;
+stack<int> S;
+
 int main() {
-	int K;
-	cin >> K;
-
-	stack<int> numbers; // 스택 자료구조 사용 , 입력 수 저장
-
-	for (int i = 0; i < K; ++i)
-	{
-		int num;
-		cin >> num; // 입력 숫자 받음
-
-		if (num == 0) {
-			if (!numbers.empty()) {
-				numbers.pop();
-			}
-        }
-		else
-		{
-			// 0이 아닌 경우 수 추가
-			numbers.push(num);
-		}
-	}
-
-	int sum = 0;
-	while (!numbers.empty())
-	{
-		sum += numbers.top(); // 맨 위를 더하고
-		numbers.pop(); // 빼버림
-		// 반복하면서 모든 수를 더함
-	}
-
-	cout << sum << "\n";
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    cin >> K;
+    while (K--)
+    {
+        int num;
+        cin >> num;
+        // 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고, 아닐 경우 해당 수를 쓴다.
+        if (num == 0) S.pop();
+        else S.push(num);
+    }
+    // 모든 수를 받아 적은 후 그 수의 합
+    while (!S.empty())
+    {
+        ans += S.top();
+        S.pop();
+    }
+    cout << ans;
 }
